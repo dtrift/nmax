cb49e2f857e0baaee3f42ef3e8a814b74ffd2574
dtrift <89174579997@mail.ru>
2020-03-27 10:38:39 +0500

    added logic

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -2,5 +2,12 @@ require "nmax/version"
 
 module Nmax
   class Error < StandardError; end
-  # Your code goes here...
+  
+  def self.parse(argv)
+    STDIN.each_line&.reduce([]) do |result, line|
+      result += line.scan(/[0-9]{1,1000}/).map(&:to_i)
+
+      result.sort.reverse[0...argv.first.to_i]
+    end
+  end
 end
