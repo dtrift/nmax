f5b25c3a46b5ace7818bf08fc441aa3143b6df67
dtrift <89174579997@mail.ru>
2020-03-27 12:55:10 +0500

    added test

--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -2,4 +2,14 @@ RSpec.describe Nmax do
   it "has a version number" do
     expect(Nmax::VERSION).not_to be nil
   end
+
+  context '.parse return' do
+    let(:data) { (0..10).map { |x| (x * 10).to_s } }
+
+    before { allow(STDIN).to receive(:each_line).and_return(data) }
+
+    it 'correct count' do
+      expect(Nmax.parse([2]).size).to eq(2)
+    end
+  end
 end
