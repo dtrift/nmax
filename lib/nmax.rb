34c4cc236674978c8331665c89ea3276e8a1833d
dtrift <89174579997@mail.ru>
2020-03-27 10:49:25 +0500

    fix

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -2,12 +2,12 @@ require "nmax/version"
 
 module Nmax
   class Error < StandardError; end
-  
+
   def self.parse(argv)
     STDIN.each_line&.reduce([]) do |result, line|
       result += line.scan(/[0-9]{1,1000}/).map(&:to_i)
 
-      result.sort.reverse[0...argv.first.to_i]
+      result.uniq.sort.reverse[0...argv.first.to_i]
     end
   end
 end
